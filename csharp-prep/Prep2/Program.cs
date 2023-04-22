99b359511b0a28e98fa53fcb5e2b68391dd81d2b
cristianodelgado <cristiandelgado061996@gmail.com>
2023-04-22 20:49:55 -0300

    Prep 2 added

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage? ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+        string letter;
+        string message;
+        if (gradePercentage >= 90)
+        {	
+             letter = "A";	
+            //Console.WriteLine("Your qualification is: A");
+        }
+        else if (gradePercentage >= 80)
+        {   
+             letter = "B";
+            //Console.WriteLine("Your qualification is: B");
+        }
+        else if (gradePercentage >= 70)
+        {
+             letter = "C";
+            //Console.WriteLine("Your qualification is: C");
+        }
+        else if (gradePercentage >= 60)
+        {
+             letter = "D";
+            //Console.WriteLine("Your qualification is: D");
+        }
+        else
+        {
+             letter = "F";
+            //Console.WriteLine("Your qualification is: F");
+        }
+
+        if (gradePercentage >= 70)
+        {
+            //Console.WriteLine("congratulations you passed!");
+             message = "congratulations you passed!";
+        }
+        else
+        {
+            //Console.WriteLine("You are close to passing. Cheer up in the next one you can do it!");
+             message = "You are close to passing. Cheer up in the next one you can do it!";
+        }
+
+        Console.WriteLine($"Your qualification is: {letter}, {message} ");
     }
 }
