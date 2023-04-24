4e938fcbae6ea44200056e1b99f240423d99e229
cristianodelgado <cristiandelgado061996@gmail.com>
2023-04-23 21:15:29 -0300

    modificar

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,6 +8,6 @@ class Program
       string firstName = Console.ReadLine();
       Console.Write("What is your last name? ");
       string lastName = Console.ReadLine();
-      Console.WriteLine($"Your name is {firstName}, {firstName}{lastName}.");
+      Console.WriteLine($"Your name is {firstName}, {firstName} {lastName}.");
     }
 }
