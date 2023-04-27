70cf0be87e6c5e753d51bf151e4ef2cb10dd8323
cristianodelgado <cristiandelgado061996@gmail.com>
2023-04-26 23:56:12 -0300

    task

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       
+        
+	    int Attempts = 7;
+	    int counter = 1;
+
+
+	    while (counter <= Attempts) 
+		{
+		
+		    Console.WriteLine("What is the magic number? ");
+    	    string input1 = Console.ReadLine();
+		    int magicNumber = int.Parse(input1);
+		
+		    Console.WriteLine("What is your guess? ");
+    		string input2 = Console.ReadLine();
+		    int yourGuess = int.Parse(input2);
+		
+		    if(magicNumber == yourGuess)
+			{
+		
+			    Console.WriteLine("You guessed it! ");
+				
+			
+
+		    }
+	
+		    if(magicNumber > yourGuess)
+			{
+		
+			    Console.WriteLine("Higher ");
+				
+			    
+
+		    }
+		    else
+			{
+
+			    Console.WriteLine("Lower");
+
+		    }
+		    counter++;
+	    }
+ 
+        
+        
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
+using System.Collections.Generic;
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a series of numbers and press 0 to finish.");
+        int Attempts = 9;
+        int counter = 1;
+        while (counter <= Attempts)
+        { 
+            Console.WriteLine("Enter a number: ");
+            string input1 = Console.ReadLine();
+            int listNumber = int.Parse(input1);
+            if (listNumber != 0)
+            {
+                numbers.Add(listNumber);
+            }
+            else
+            {
+              break;   
+            }
+        }
+        float sum = 0;
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+
+        float average = sum/ numbers.Count;
+        int max = numbers[0];
+        foreach (int n in numbers)
+        {
+            if (n > max)
+            {
+                max = n;
+            }
+        }
+        Console.WriteLine($"The sum is:  {sum}");
+        Console.WriteLine($"The average is:  {average}");
+        Console.WriteLine($"The Max is:  {max}");
+        
+    
     }
 }
