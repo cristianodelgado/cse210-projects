b66bff291da8db53570ecba0f92f2742783becbc
cristianodelgado <cristiandelgado061996@gmail.com>
2023-05-06 22:17:56 -0300

    learning02

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,20 @@
+using System;
+public class  Job
+{ 
+    public string _company;
+    public string _jobTitle;
+
+    public int _startYear;
+
+    public int _endYear;
+
+    public void DisplayJob()
+    {
+        Console.WriteLine($"({_company}) {_jobTitle} {_startYear} - {_endYear}");
+    }
+
+    
+
+
+
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Bulonera Camba SRL";
+        job1._jobTitle = "Administrativo";
+        job1._startYear = 2014;
+        job1._endYear = 2021;
+        job1.DisplayJob();
+    
+    
+        Job job2 = new Job();
+        job2._company = "Degma SRL";
+        job2._jobTitle = "Comprador";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        job2.DisplayJob();
+
+        
+        Resume myResume = new Resume();
+        myResume._name = "Cristian Delgado";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+
+        Console.WriteLine();
+        myResume.Display();
+
+    
     }
 }
